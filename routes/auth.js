bb892f4e8ca3bdef69da845e47872d3e2487ecbf
Muembia <jonathanmuembia3@gmail.com>
2026-06-05 18:09:41 +0200

    fix: update GitHub callback route to improve session handling and error redirection

--- routes/auth.js
+++ routes/auth.js
@@ -6,8 +6,22 @@ const authRouter = express.Router();
 
 authRouter.get('/login', authController.ensureGitHubConfigured, authController.passport.authenticate('github', { scope: ['user:email'] }));
 
-authRouter.get('/callback', authController.passport.authenticate('github', { failureRedirect: '/' }), authController.githubCallback);
-
+authRouter.get('/auth/github/callback', 
+    authController.passport.authenticate('github', { failureRedirect: '/api-docs', session: true }),
+    (req, res, next) => {
+        // 1. Assign the user data to the session
+        req.session.user = req.user;
+        
+        // 2. FORCE the session to save to memory/database before moving on
+        req.session.save((err) => {
+            if (err) {
+                return next(err);
+            }
+            // 3. ONLY redirect once we are 100% sure the session is stored safely
+            res.redirect('/');
+        });
+    }
+);
 authRouter.post('/logout', authController.logout);
 
 
